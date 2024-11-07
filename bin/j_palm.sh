dc6555b89a3fb5533b95c93cfc987cb8acceefaa
ainarajn <162466663+ainarajn@users.noreply.github.com>
2024-11-07 12:46:03 +0100

    Add files via upload

---
+++ bin/j_palm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+module load Python/3.9.6-GCCcore-11.2.0
+
+PATH_TO_PALM=/homes/users/ajimenez/scratch/palm
+METADATA=$1
+PATH_TO_LIK=$2
+MAXPVAL=$3
+
+#Obtain file with minimum number of independent, significant SNPs in each GWAS
+awk -F'\t' 'BEGIN {OFS="\t"} NR==1 {for (i=1; i<=NF; i++) if ($i ~ /^min_independent_significant_SNPs@/) header[i]=$i} NR==2 {for (i=1; i<=NF; i++) if (header[i]) print header[i], $i}' \
+$1 > $(basename "$1" _selected_SNPs.tsv)_significant_independent_SNPs.txt
+
+#Separate the GWAS in the name of the file
+INFO=($(echo "$1" | tr '-' ' '))
+#Modify the elements so that they match the names in the file
+GWAS_1="${INFO[0]}.tsv"
+GWAS_2="${INFO[1]%_selected_SNPs.tsv}.tsv"
+
+#Call the script
+python $PATH_TO_PALM/palm_custom.py \
+    --traitDir ${PATH_TO_LIK}/ \
+    --metadata $METADATA \
+    --maxp $MAXPVAL \
+    --B 10000 \
+    --traits $GWAS_1,$GWAS_2 \
+    > $(basename "$1" _selected_SNPs.tsv)_J_PALM.txt

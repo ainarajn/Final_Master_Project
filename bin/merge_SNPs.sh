ff9777c46f74a1141da8865ce54affa9d240b7ad
ainarajn <162466663+ainarajn@users.noreply.github.com>
2024-11-18 11:04:05 +0100

    Update merge_SNPs.sh

--- bin/merge_SNPs.sh
+++ bin/merge_SNPs.sh
@@ -4,19 +4,19 @@
 #subsequently removes any duplicate entries
 
 # Define input
-INPUT=$1
+INPUT=("$@")
 
 # Sort the input by the 5th column removing duplicates and stores it in a file
-sort -k5,5 -u "${INPUT}" > merged_SNPs.tsv
+sort -k5,5 -u "${INPUT[@]}" > merged_SNPs.tsv
 
 # Captures the last line, which refers to the header
-headers=\$(tail -n 1 merged_SNPs.tsv)
+headers=$(tail -n 1 merged_SNPs.tsv)
 
 # Removes the last line of the file by printing in a temporaty file the previous line for each iteration
-awk 'NR>1{print last} {last=\$0}' merged_SNPs.tsv > temp_file.tsv
+awk 'NR>1{print last} {last=$0}' merged_SNPs.tsv > temp_file.tsv
 
 # Rebuild the file with the header on top
-echo -e "\$headers\n\$(cat temp_file.tsv)" > merged_SNPs.tsv
+echo -e "$headers\n$(cat temp_file.tsv)" > merged_SNPs.tsv
 
 # Remove temporary file
 rm temp_file.tsv
